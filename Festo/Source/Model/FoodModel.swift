2c31f4fa2826bc3a80f145c2c53794b78de0570e
maahdisousa <mmdisousa@gmail.com>
2022-11-23 11:48:01 -0300

    make model file

---
+++ Festo/Source/Model/FoodModel.swift
@@ -0,0 +1,17 @@
+//
+//  ComidasModel.swift
+//  Festo
+//
+//  Created by Marília de Sousa on 23/11/22.
+//
+
+import Foundation
+
+struct Food {
+    
+    let name: String
+    let unitWeight: Int
+    let imageFood: String
+    
+}
+
