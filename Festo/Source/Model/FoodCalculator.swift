b8321cd513e052c88ee0334c86fc177a656a26e5
Stephane <stephanegiraolinhares@gmail.com>
2022-11-29 11:51:54 -0300

    Refactored calculator using a for

--- Festo/Source/Model/FoodCalculator.swift
+++ Festo/Source/Model/FoodCalculator.swift
@@ -9,26 +9,36 @@ import Foundation
 
 struct FoodCalculator {
     var numberGuests: Int = 30
-    var foodsModel = FoodModel(name: "Salgado", unitWeight: 30)
+    var salgadinho = FoodModel(name: "Salgado", unitWeight: 30)
 
     func calculator(numberGuests: Int, selectFood: [FoodModel] ) -> [FoodGuest] {
 
         let totalWeightGuest = 400
         var weightTipe: Double = Double(400 / selectFood.count)
-//        var units: Int = Int(weightTipe) / foodsModel.unitWeight
         
         var units: [Int] =  selectFood.map { food in
             Int(weightTipe)/Int(food.unitWeight)
         }
-        var rest: [Int] = selectFood.map { food in
+        var unitWeights: [Double] = selectFood.map { weight in Double(weight.unitWeight)}
+        
+        var rests: [Int] = selectFood.map { food in
             Int(weightTipe) % Int(food.unitWeight)
         }
+        
+        for index in 0..<units.count {
+            let unitWeight = unitWeights[index]
+            let rest = rests[index]
+            
+            let minPercent = 0.6
+            let acceptUnitWeight = unitWeight * minPercent
+           
+            if Double(rest) >= Double(acceptUnitWeight) {
+                units[index] += 1
+            }
 
-//        if(rest.map { rest in I} >= (Int(0.6) * foodsModel.unitWeight)) {
-//            units = units + 1
-//        }
-        var guest = [FoodGuest(food: <#T##FoodModel#>, unitForGuest: <#T##Int?#>, weightForGuest: <#T##Int?#>, totalWeight: <#T##Int?#>, totalUnit: <#T##Int?#>)]
-        return guest
+            
+        }
+        return []
     }
 
 }
